4dbb479636587859e5d11831eb81cf0b6393c33e
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-05-24 14:51:40 +0200

    initial works

--- source/models/hcm/entities/package.d
+++ source/models/hcm/entities/package.d
@@ -1,1 +1,8 @@
 module models.hcm.entities;
+
+public import models.hcm.entities.bi;
+public import models.hcm.entities.compensations;
+public import models.hcm.entities.employments;
+public import models.hcm.entities.jobs;
+public import models.hcm.entities.positions;
+public import models.hcm.entities.workers;
